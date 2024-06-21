d075a098bc9f4b48433c94a84f8d6d250686e8eb
Andrew Talaboc <mrseniax@gmail.com>
2024-06-21 14:32:37 +0800

    U[date

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,21 @@ import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 export default function Home() {
   const words = [
     {
-      text: "Allanah",
+      text: "asdasd",
     },
     {
-      text: "Ranuco,",
+      text: "asdasd,",
     },
     {
       text: "I",
     },
     {
-      text: "miss",
+      text: "asdsad",
     },
     {
       text: "you.",
     },
-    { text: " ❤️" },
+    { text: "asdasd" },
   ];
 
   return (
